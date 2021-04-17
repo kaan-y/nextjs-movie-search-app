3a9901ff7d83e78dc6240be6fe447ed4ba71a23a
Kaan Yıldız <kn.yildiz@gmail.com>
2021-04-17 22:00:17 +0300

    initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+  reactStrictMode: true,
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src')
+
+    return config
+  },
+}
